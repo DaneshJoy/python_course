94e4bacfa94159d17155d26c454eac48ef0b943b
Saeed Mohagheghi <saeed.discovery@gmail.com>
2021-11-24 19:25:54 +0330

    finished project ghore_keshi_v2 + exe

--- Projects/5_ghore_keshi_v2/main.py
+++ Projects/5_ghore_keshi_v2/main.py
@@ -1,13 +1,13 @@
 '''
 Barname Ghore Keshi (version 2)
 '''
+import os
 import random
 import time
 import my_logos
 from datetime import datetime
+from persiantools.jdatetime import JalaliDateTime
 
-
-today = datetime.now()
 
 def print_text(text):
     for c in text:
@@ -16,14 +16,18 @@ def print_text(text):
     print()
 
 
-# Pick random logo
-logo_list = [my_logos.logo1, my_logos.logo2, my_logos.logo3]
-logo = random.choice(logo_list)
+print(my_logos.logo1 + '\n')
 
-print(logo + '\n')
+today_miladi = datetime.now()
+today_shamsi = JalaliDateTime.now()
+# Method 1
+# print(f'Emrooz: {today_miladi.year}/{today_miladi.month:0>2}/{today_miladi.day:0>2}' +
+#       f' {today_miladi.hour:0>2}:{today_miladi.minute:0>2}')
 
-print(f'Emrooz: {today.year}/{today.month:0>2}/{today.day:0>2}' +
-      f' {today.hour:0>2}:{today.minute:0>2}')
+# Method 2
+print(today_miladi.strftime('Emrooz: %Y/%m/%d %H:%M:%S'))
+print(today_shamsi.strftime('Emrooz: %Y/%m/%d %H:%M:%S'))
+print(today_shamsi.strftime('%c'))
 
 print_text('Asaami ra yeki yeki vared karde va Enter bezanid')
 print_text('Naamhaye tekrari be soorate khodkar hazf mishavand')
@@ -58,6 +62,14 @@ while True:
 
 # %% Entekhaabe Barandegan
 
+# Clear screen
+if os.name == 'nt':
+    _ = os.system('cls')  # Clear screen in Windows CMD
+else:
+    _ = os.system('clear')  # Clear screen in Linux/Mac
+
+print(my_logos.logo2 + '\n')
+
 winners = random.sample(names_list, n)
 # winners = '\n'.join(winners)
 print('>>> Asami Barandegan:\n')
@@ -67,3 +79,5 @@ for name in winners:
         print(c, end='', flush=True)
         time.sleep(0.1)
     print(' ☺')
+    
+input()
