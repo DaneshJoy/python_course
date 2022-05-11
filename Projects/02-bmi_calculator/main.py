e49cc4337ead02ca44f6afdc9204d39ab30f0d1a
Saeed Mohagheghi <saeed.discovery@gmail.com>
2022-05-11 15:04:59 +0430

    modified slides 2 & 3 and projects 1 & 2 codes

--- Projects/02-bmi_calculator/main.py
+++ Projects/02-bmi_calculator/main.py
@@ -1,14 +1,14 @@
 
 print('''
-ooo        ooooo                  oooooooooo.  ooo        ooooo ooooo 
-`88.       .888'                  `888'   `Y8b `88.       .888' `888' 
- 888b     d'888  oooo    ooo       888     888  888b     d'888   888  
- 8 Y88. .P  888   `88.  .8'        888oooo888'  8 Y88. .P  888   888  
- 8  `888'   888    `88..8'         888    `88b  8  `888'   888   888  
- 8    Y     888     `888'          888    .88P  8    Y     888   888  
-o8o        o888o     .8'          o888bood8P'  o8o        o888o o888o 
-                 .o..P'                                               
-                 `Y8P'                                                 
+ooo        ooooo                  oooooooooo.  ooo        ooooo ooooo
+`88.       .888'                  `888'   `Y8b `88.       .888' `888'
+ 888b     d'888  oooo    ooo       888     888  888b     d'888   888
+ 8 Y88. .P  888   `88.  .8'        888oooo888'  8 Y88. .P  888   888
+ 8  `888'   888    `88..8'         888    `88b  8  `888'   888   888
+ 8    Y     888     `888'          888    .88P  8    Y     888   888
+o8o        o888o     .8'          o888bood8P'  o8o        o888o o888o
+                 .o..P'
+                 `Y8P'
                                                   ''')
 
 # Daryafte Ghad va Vazne Kaarbar
@@ -16,7 +16,7 @@ w = input("Please enter your weight (in kg): ")
 h = input("Please enter your height (in cm): ")
 
 # Error handling (check if input is digit)
-if w.replace("."," ").isdigit() and h.replace("."," ").isdigit():
+if w.replace(".", " ").isdigit() and h.replace(".", " ").isdigit():
     w = float(w)
     h = float(h)
     h /= 100
@@ -28,13 +28,15 @@ if w.replace("."," ").isdigit() and h.replace("."," ").isdigit():
     # Gozaresh be karbar
     print(f"Your BMI is: {bmi} kg/m^2 because your weight is {w} and your height is {h*100}")
 
-    if bmi<18.5:
+    # Tamrin2: Classhaye chaaghi ra ham be sharthaaye zir ezafe konid
+    if bmi < 18.5:
         print("Shoma Underweight Hastid")
-    elif 18.5<=bmi<24.9:
+    elif 18.5 <= bmi < 24.9:
         print("Tabrik, Shoma Normal Hastid")
     else:
         print("Shoma Chagh Hastid")
-        
-else: 
+
+    # Tamrin3: Meghdaare ezafe ya kamboode vazn ra ham gozaresh dahid
+
+else:
     print("Invalid input: Bayad addad vared konid")
-    
