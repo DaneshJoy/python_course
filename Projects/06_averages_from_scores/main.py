853f52f62e5144910a892d05c4b6c23430a538ef
Saeed Mohagheghi <saeed.discovery@gmail.com>
2022-06-01 18:23:33 +0430

    updated project 6 code

--- Projects/06_averages_from_scores/main.py
+++ Projects/06_averages_from_scores/main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from collections import Counter 
 from text_reader import get_data_from_text_file
 import pprint
@@ -18,24 +18,26 @@ for filename in list_of_files:
     try:
         with open(filepath, 'r') as f:
             my_data = f.readlines()
-            scores_dict = get_data_from_text_file(my_data)
-    
-            scores = scores_dict.values()
-            average = sum(scores) / len(scores)
-            
-            # Extract student name from filename
-            student_name = filename.split('.')[0]
-            
-            result_dict[student_name] = round(average, 2)
-            
-            print(f'\033[95m {student_name}: \033[92m{average:0.2f} \033[0m')
     except:
         print(f'Error reading from file: {filepath}')
+        sys.exit()
+
+    scores_dict = get_data_from_text_file(my_data)
+    scores = scores_dict.values()
+    average = sum(scores) / len(scores)
+
+    # Extract student name from filename
+    student_name = filename.split('.')[0]
+
+    result_dict[student_name] = round(average, 2)
+
+    print(f'\033[95m {student_name}: \033[92m{average:0.2f} \033[0m')
+            
 # Sort results dictionary (Descending)
 result_sorted = Counter(result_dict).most_common()
 # Ascending results using reverse method
 # result_sorted.reverse()
-ppt.pprint(result_sorted)
+# ppt.pprint(result_sorted)
 
 # %% Write results to a file
 out_file = 'results.txt'
