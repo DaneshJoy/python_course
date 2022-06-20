0659286412ef599b0465a8651b3219bb49d90839
Saeed Mohagheghi <saeed.discovery@gmail.com>
2022-06-20 17:03:08 +0430

    added final project files

---
+++ final_project/image_lab.py
@@ -0,0 +1,22 @@
+# Your imports here
+
+
+class ImageLab:
+    '''
+    Image operations for rgb images
+    Methods:
+        - Read image
+        - Change Brightness/Contrast/Color
+        - Show image
+        - Save image
+    '''
+    pass
+
+
+class ImageLabGray(ImageLab):
+    '''
+    ImageLab methods and properties + Auto-contrast for gray images
+    Method:
+        - Pillow Auto-contrast
+    '''
+    pass
---
+++ final_project/main.py
@@ -0,0 +1,10 @@
+# Your imports here
+
+
+# Read info file (csv or xlsx)
+# Read images
+# Check image mode from info file
+# if mode=='rgb'  --> Use ImageLab class to change brightness/contrast
+# if mode=='gray' --> Use ImageLabGray class to auto contrast
+# Show images and results side by side (before/after)
+# Save results in a separate folder (results)
