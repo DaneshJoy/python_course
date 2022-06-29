805c65e706fbaf478fc63ca3c1fd4b6fecb7c68e
Saeed Mohagheghi <saeed.discovery@gmail.com>
2022-06-29 17:17:16 +0430

    changed final project comments

--- final_project/main.py
+++ final_project/main.py
@@ -4,7 +4,7 @@
 # Read info file (csv or xlsx)
 # Read images
 # Check image mode from info file
-# if mode=='rgb'  --> Use ImageLab class to change brightness/contrast
-# if mode=='gray' --> Use ImageLabGray class to auto contrast
+# if mode=='rgb'  --> Use ImageEnhancer class to change brightness/contrast
+# if mode=='gray' --> Use ImageEnhancerGray class to auto contrast
 # Show images and results side by side (before/after)
 # Save results in a separate folder (results)
